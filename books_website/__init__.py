29a9347774f16e4c49fc88766aa7d960af0236be
setevoybog <15861500+setevoybog@users.noreply.github.com>
2020-10-22 18:48:21 +0300

    implemented navigation between pages

--- books_website/__init__.py
+++ books_website/__init__.py
@@ -7,6 +7,8 @@ def create_app(config_class=Config):
     app.config.from_object(Config)
 
     from books_website.main.routes import main
+    from books_website.users.routes import users
     app.register_blueprint(main)
+    app.register_blueprint(users)
 
     return app
