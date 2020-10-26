b40c4f2f302df8dcfdd342305569679fdd834b36
setevoybog <15861500+setevoybog@users.noreply.github.com>
2020-10-26 09:10:01 +0300

    add titles to routes parameters

--- books_website/users/routes.py
+++ books_website/users/routes.py
@@ -5,9 +5,9 @@ users = Blueprint('users', __name__)
 
 @users.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', title='Login')
 
 
 @users.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('register.html', title='Register')
